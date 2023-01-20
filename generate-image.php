82bd403ac1fb68d151c8c1b60f9d92699340291f
junaid051 <mail.imagistudios@gmail.com>
2023-01-20 11:50:04 +0530

    php

--- generate-image.php
+++ generate-image.php
@@ -1,19 +1,35 @@
 <?php
 
+$prompt = $_POST['prompt'];
+$width = 512;
+$height = 512;
+$steps = 50;
+$numberOfImages = 1;
+$seed = 123456789;
+
 $curl = curl_init();
 
 curl_setopt_array($curl, [
-  CURLOPT_URL => "https://api.leapml.dev/api/v1/images/models",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "GET",
-  CURLOPT_HTTPHEADER => [
-    "accept: application/json",
-    "authorization: Bearer 7751649d-ae90-4cb0-8ec6-f63d8e74fe3c"
-  ],
+    CURLOPT_URL => "https://api.leapml.dev/api/v1/images/models/8b1b897c-d66d-45a6-b8d7-8e32421d02cf/inferences",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => json_encode(array(
+        "prompt" => $prompt,
+        "width" => $width,
+        "height" => $height,
+        "steps" => $steps,
+        "numberOfImages" => $numberOfImages,
+        "seed" => $seed
+    )),
+    CURLOPT_HTTPHEADER => [
+        "accept: application/json",
+        "content-type: application/json",
+        "authorization: Bearer 7751649d-ae90-4cb0-8ec6-f63d8e74fe3c"
+    ],
 ]);
 
 $response = curl_exec($curl);
@@ -22,7 +38,12 @@ $err = curl_error($curl);
 curl_close($curl);
 
 if ($err) {
-  echo "cURL Error #:" . $err;
+    echo "cURL Error #:" . $err;
 } else {
-  echo $response;
+    $json = json_decode($response, true);
+    if($json['status'] === 'success') {
+        echo $json['data']['url'];
+    } else {
+        echo $json['message'];
+    }
 }
