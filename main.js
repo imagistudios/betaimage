ee8b60ae743348f10048ffba01a29766b4c6712e
junaid051 <mail.imagistudios@gmail.com>
2023-01-20 11:53:32 +0530

    upd2

--- main.js
+++ main.js
@@ -1,33 +1,19 @@
 function generateImage() {
-    var text = document.getElementById("text-area").value;
-    var imageContainer = document.getElementById("image-container");
-
-    var data = {
-        "prompt": text,
-    }
-    var headers = new Headers({
-        'Content-Type': 'application/json',
-    });
-    var options = {
-        method: 'POST',
-        headers,
-        body: JSON.stringify(data),
-    }
-    fetch('generate-image.php', options)
-        .then(response => {
-            if (response.ok) {
-                return response.text();
-            } else {
-                throw new Error('Request failed');
-            }
-        })
-        .then(imageUrl => {
-            var img = document.createElement("img");
-            img.src = imageUrl;
-            imageContainer.appendChild(img);
-        })
-        .catch(error => {
-            console.log(error);
-            alert('Request failed. Please check the console for more details.');
-        });
+    document.getElementById("text-input").submit();
 }
+
+document.getElementById("text-input").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var imageContainer = document.getElementById("image-container");
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", this.action);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var img = document.createElement("img");
+            img.src = xhr.responseText;
+            imageContainer.appendChild(img);
+        }
+    }
+    xhr.send(new FormData(this));
+});
